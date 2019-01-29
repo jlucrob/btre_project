f1c51daca06b5f7c1aecefa6ec4f43a53ab2ae0b
unknown <jlucrob@hotmail.com>
2019-01-28 23:30:16 -0500

    added local_settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='jlucrobi@gmail.com'
 EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass 
+    
