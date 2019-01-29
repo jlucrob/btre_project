a2aef431b595f634c2a94908a422cde54aa64089
unknown <jlucrob@hotmail.com>
2019-01-28 23:37:09 -0500

    removed email settings

--- btre/settings.py
+++ btre/settings.py
@@ -143,13 +143,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='jlucrobi@gmail.com'
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
